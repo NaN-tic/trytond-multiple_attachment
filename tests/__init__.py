187b6c4eae081d4f976ef61286fbf40c9a569258
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:19:56 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part of the multiple_attachment module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-from .test_multiple_attachment import suite
-
-__all__ = ['suite']
